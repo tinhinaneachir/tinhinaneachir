4563f73396f266c8a3430e9ddf2e785d2ba6066f
Tina Achir <tinhinaneachir@gmail.com>
2022-01-25 22:10:25 +0100

    configration base de donnees

--- src/com/supdevinci/videostreaming/dao/Dao.java
+++ src/com/supdevinci/videostreaming/dao/Dao.java
@@ -6,15 +6,37 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import javax.annotation.Resource;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
 public class Dao {
 	
+
+	  private DataSource dataSource;
+	  
+	
+	
 	protected Connection ouvrirConnexion() throws SQLException {
-		return DriverManager.getConnection("jdbc:mysql://localhost:3306/vs_videostreaming?"  
-							+ "useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root","root");
+		try {
+
+			System.setProperty("h2.clientTraceDirectory", "/h2db");
+			Context initCtx = new InitialContext();
+			Context envCtx = (Context) initCtx.lookup("java:comp/env");
+			DataSource ds = (DataSource)
+			  envCtx.lookup("vs-database");
+			return ds.getConnection();
+		} catch (Exception e) {
+			return null;
+		}
+		//return DriverManager.getConnection("jdbc:mysql://localhost:3306/vs_videostreaming?"  
+		//					+ "useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root","root");
 	}
 	
 	protected void chargerDriver() throws ClassNotFoundException {
-		Class.forName("com.mysql.jdbc.Driver");
+		//Class.forName("com.mysql.jdbc.Driver");
 	}
 	
 	protected void fermerConnexion(Connection connexion, Statement statement, ResultSet resultat) {
